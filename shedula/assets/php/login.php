dd3cfb6f97b4660df0c32179a7788f46f948a353
Ian Wach <iankaveli0@gmail.com>
2023-02-27 19:44:44 +0300

    Added some files in the Php Directory

---
+++ shedula/assets/php/login.php
@@ -0,0 +1,21 @@
+<?php include("./includes/header.php")?>
+    <head>
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <section style="height:80vh;">
+        <div>
+            <form>
+                <div>
+                    <label name="e-mail">Email Address</label>
+                    <input type="email" name="e-mail" placeholder="Enter Email"/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter Password"/>
+                </div>
+                <input class="" type="Submit"/>
+            </form>
+            <div><a href="./signup.php">Don't Have Account?</a></div>
+        </div>
+    </section>
+<?php include("./includes/footer.php")?>
---
+++ shedula/assets/php/signup.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/signup.css">
+    <title>Shedula Application - Login Page</title>
+</head>
+<body>
+    <?php include("./includes/header.php") ?>
+    <section class="signup-section">
+        <div class="container">
+            <form class="signup-form">
+                <div>
+                    <label name="frname">First Name</label>
+                    <input class="" id="" type="name" name="frname" placeholder="First Name" \>
+                </div>
+                <div>
+                    <label class="" id="" name="lname">Last Name</label>
+                    <input class="" id="" name="lname" type="name" placeholder="Last Name" \>
+                </div>
+                <div>
+                    <label class="" name="e-mail" id="">Email Address</label>
+                    <input class="" id="" type="email" name="e-mail" placeholder="Your Email" />
+                </div>
+                <div>
+                    <label name="pass">Password</label>
+                    <input class="" name="pass" type="password" placeholder="Enter Password"/>
+                </div>
+                <div>
+                    <label name="cpass">Confirm Password</label>
+                    <input class="" name="cpass" type="password" placeholder="Confirm Password"/>
+                </div>
+                <input class ="btn" type="submit"/>
+            </form>
+        </div>
+    </section>
+    <?php include("./includes/footer.php") ?>
+</body>
+</html>
