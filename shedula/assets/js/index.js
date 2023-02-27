1f48161941997c47e0a073536d34d738fc5efe4f
Ian Wach <iankaveli0@gmail.com>
2023-02-27 19:46:37 +0300

    Added and Modified a JS File

--- shedula/assets/js/index.js
+++ shedula/assets/js/index.js
@@ -2,9 +2,7 @@
 
 function validateForm(){
     var userName = document.getElementsById("d-name").value;
-    var eMail = document.getElementsByName("E-mail");
-    if(userName = ""){
-        alert("Kindly Enter your Name To Proceed")
-        return false;
+    if(userName == ""){
+        alert("Kindly Enter your Name To Proceed");
     }
 }
