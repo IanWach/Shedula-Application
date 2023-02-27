1f59353157dcb9b44aca00ef5fbdea68db98a1b1
Ian Wach <iankaveli0@gmail.com>
2023-02-27 19:45:19 +0300

    Added Php Files in the Include Directory

---
+++ shedula/assets/php/includes/footer.php
@@ -0,0 +1,133 @@
+<head>
+    <link rel="stylesheet" href="/shedula/assets/css/style.css">
+</head>
+
+
+<footer class="footer" style="position:absolute;">
+
+<div class="footer-top">
+  <div class="container">
+    <div class="footer-brand">
+      <a href="#" class="logo"><ion-icon name="time-outline"></ion-icon> Shedula</a>
+      <p class="footer-text">
+        A great platform to add, plan and schedule your time in a Private and Secure way.
+      </p>
+    </div>
+    <ul class="footer-list">
+      <li>
+        <p class="footer-list-title">Company</p>
+      </li>
+      <li>
+        <a href="#" class="footer-link">
+          <ion-icon name="chevron-forward"></ion-icon>
+          <span class="span">About us</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="footer-link">
+          <ion-icon name="chevron-forward"></ion-icon>
+          <span class="span">Services</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="footer-link">
+          <ion-icon name="chevron-forward"></ion-icon>
+          <span class="span">Pricing</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="footer-link">
+          <ion-icon name="chevron-forward"></ion-icon>
+          <span class="span">Blog</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="footer-link">
+          <ion-icon name="chevron-forward"></ion-icon>
+          <span class="span">Login</span>
+        </a>
+      </li>
+    </ul>
+    <ul class="footer-list">
+      <li>
+        <p class="footer-list-title">Useful Links</p>
+      </li>
+      <li>
+        <a href="#" class="footer-link">
+          <ion-icon name="chevron-forward"></ion-icon>
+          <span class="span">Terms of Services</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="footer-link">
+          <ion-icon name="chevron-forward"></ion-icon>
+          <span class="span">Privacy Policy</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="footer-link">
+          <ion-icon name="chevron-forward"></ion-icon>
+          <span class="span">Listing</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="footer-link">
+          <ion-icon name="chevron-forward"></ion-icon>
+          <span class="span">Contact</span>
+        </a>
+      </li>
+    </ul>
+    <ul class="footer-list">
+      <li>
+        <p class="footer-list-title">Contact Details</p>
+      </li>
+      <li class="footer-item">
+        <ion-icon name="location-outline"></ion-icon>
+        <address class="address">
+          Molo-Nakuru,<br>
+          Kenya,<br>
+          768 - 20106
+          <a href="#" class="address-link">View on Google map</a>
+        </address>
+      </li>
+      <li class="footer-item">
+        <ion-icon name="mail-outline"></ion-icon>
+        <a href="mailto:contact@shedula.app" class="footer-link">contact@shedula.app</a>
+      </li>
+      <li class="footer-item">
+        <ion-icon name="call-outline"></ion-icon>
+        <a href="tel:+254534468854" class="footer-link">+254 534-468-854</a>
+      </li>
+    </ul>
+  </div>
+</div>
+<div class="footer-bottom">
+  <div class="container">
+    <p class="copyright">
+      &copy; 2023 Shedula. All Right Reserved by <a href="#" class="copyright-link">comtechconsultancy</a>.
+    </p>
+    <ul class="social-list">
+      <li>
+        <a href="#" class="social-link">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="social-link">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="social-link">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="social-link">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a>
+      </li>
+    </ul>
+  </div>
+</div>
+</footer>
---
+++ shedula/assets/php/includes/header.php
@@ -0,0 +1,46 @@
+<head>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<header style="position: relative;" class="header" data-header>
+    <div class="container">
+        <a href="#" class="logo">
+            <ion-icon class="logo-icon" name="time-outline"></ion-icon> Shedula
+        </a>
+        
+        <nav class="navbar container" data-navbar>
+            <ul class="navbar-list">
+
+                <li>
+                    <a href="#" class="navbar-link" data-nav-link>Home</a>
+                </li>
+
+                <li>
+                    <a href="#" class="navbar-link" data-nav-link>Account</a>
+                </li>
+
+                <li>
+                    <a href="/shedula/index.html#featured" class="navbar-link" data-nav-link>Schedules</a>
+                </li>
+
+                <li>
+                    <a href="/shedula/index.html#purpose" class="navbar-link" data-nav-link>About</a>
+                </li>
+
+                <li>
+                    <a href="/shedula/index.html#subscribe" class="navbar-link" data-nav-link>Contact</a>
+                </li>
+            </ul>
+        </nav>
+        
+        <a href="#" class="btn btn-secondary">Sign Up</a>
+        <button class="nav-toggle-btn" aria-label="Toggle menu" data-nav-toggler>
+            <ion-icon name="menu-outline" aria-hidden="true" class="menu-icon"></ion-icon>
+            <ion-icon name="close-outline" aria-hidden="true" class="close-icon"></ion-icon>
+        </button>
+    </div>
+</header>
+<!--
+	- ionicon link
+	-->
+	<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+	<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
