1ed9aa1f72d62839aa3bd9b5f3617ad50dbe3e65
Ian Wach <iankaveli0@gmail.com>
2023-02-28 20:33:16 +0300

    Additional Edits to the Sign Up Details

--- shedula/assets/php/signup.php
+++ shedula/assets/php/signup.php
@@ -11,32 +11,52 @@
     <?php include("./includes/header.php") ?>
     <section class="signup-section">
         <div class="container">
-            <form class="signup-form">
+            <h1>SIGNUP FORM</h1>
+            <form class="signup-form" action="./signup.php" method="post">
                 <div class="signup">
-                    <label name="frname">First Name</label>
-                    <input class="" id="" type="name" name="frname" placeholder="First Name" \>
+                    <label class="signup-label" name="frname">First Name</label>
+                    <input class="signup-input" id="" type="name" name="frname" placeholder="First Name" \>
                 </div>
                 <div class="signup">
-                    <label class="" id="" name="lname">Last Name</label>
-                    <input class="" id="" name="lname" type="name" placeholder="Last Name" \>
+                    <label class="signup-label" id="" name="lname">Last Name</label>
+                    <input class="signup-input" id="" name="lname" type="name" placeholder="Last Name" \>
                 </div>
                 <div class="signup">
-                    <label class="" name="e-mail" id="">Email Address</label>
-                    <input class="" id="" type="email" name="e-mail" placeholder="Your Email" />
+                    <label class="signup-label" name="e-mail" id="">Email Address</label>
+                    <input class="signup-input" id="" type="email" name="e-mail" placeholder="Your Email" />
                 </div>
                 <div class="signup">
-                    <label name="pass">Password</label>
-                    <input class="" name="pass" type="password" placeholder="Enter Password"/>
+                    <label class="signup-label" name="pass">Password</label>
+                    <input class="signup-input" name="pass" type="password" placeholder="Enter Password"/>
                 </div>
                 <div class="signup">
-                    <label name="cpass">Confirm Password</label>
-                    <input class="" name="cpass" type="password" placeholder="Confirm Password"/>
+                    <label class="signup-label" name="cpass">Confirm Password</label>
+                    <input class="signup-input" name="cpass" type="password" placeholder="Confirm Password"/>
                 </div>
                 <input class ="btn" type="submit"/>
+                <a class="signup-account" href="./login.php">I Have an Account</a>
             </form>
         </div>
-        <div><a href="./login.php">Have an Account?</a></div>
     </section>
+    <?php 
+    $error =NULL;      
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $FirstName = $_POST['frname'];
+            $LastName  = $_POST['lname'];
+            $EmailAddress = $_POST['e-mail'];
+            $Password = $_POST['pass'];
+            $ConfirmPassword = $_POST['cpass'];
+        //Conditions to ensure no empty Inputs and Short attributes
+        if ($Password != $ConfirmPassword)
+        echo "The Passwords do Not Match";
+        elseif ($FirstName == "" || $LastName == "")
+        echo "Enter the names correctly";
+        else
+        echo "Proceed";
+        echo $Password;
+        }
+    
+    ?>
     <?php include("./includes/footer.php") ?>
 </body>
 </html>
